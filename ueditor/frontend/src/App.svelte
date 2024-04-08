a501192dc1761119a0223d75b4331a64d9075671
Mark Hall <mark.hall@work.room3b.eu>
2024-04-08 16:55:12 +0100

    Started UI layout work

--- ueditor/frontend/src/App.svelte
+++ ueditor/frontend/src/App.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import TipTapEditor from "./lib/TipTapEditor.svelte";
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+
+  import Root from "./Root.svelte";
+
+  const queryClient = new QueryClient();
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <TipTapEditor />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
+<QueryClientProvider client={queryClient}>
+  <Root/>
+</QueryClientProvider>
---
+++ ueditor/frontend/src/Root.svelte
@@ -0,0 +1,21 @@
+<script lang="ts"></script>
+
+<svelte:head>
+  <title>uEditor</title>
+</svelte:head>
+
+<main class="flex flex-col w-screen h-screen overflow-hidden">
+  <nav aria-label="Toolbar" class="bg-blue-100">Editor Toolbar</nav>
+  <div class="flex flex-row flex-1">
+    <nav aria-label="Files" class="w-3/12 bg-red-100">
+      Navigation
+    </nav>
+    <div class="flex-1 bg-green-100">
+      Editor Content
+    </div>
+    <div class="w-3/12 bg-orange-100">
+      Editor Sidebar
+    </div>
+  </div>
+  <footer class="bg-yellow-100">Editor footer</footer>
+</main>
