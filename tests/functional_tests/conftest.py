c0ea958947a75d45b08e685001e2fc833a3b222c
Mark Hall <mark.hall@work.room3b.eu>
2024-04-15 16:26:17 +0100

    Refactored tests

---
+++ tests/functional_tests/conftest.py
@@ -0,0 +1,55 @@
+"""Test fixtures."""
+
+import os
+from typing import Generator
+
+from fastapi import FastAPI
+from pytest import fixture
+
+from ueditor import app
+from ueditor.settings import init_settings
+
+
+def app_generator(root: str) -> Generator[FastAPI, None, None]:
+    cwd = os.getcwd()
+    try:
+        os.chdir(root)
+        yield app
+    finally:
+        os.chdir(cwd)
+
+
+@fixture
+def empty_app() -> Generator[FastAPI, None, None]:
+    """Yield an empty application."""
+    try:
+        init_settings.base_path = "tests/fixtures/empty"
+        init_settings.test = True
+        yield app
+    finally:
+        init_settings.base_path = "./"
+        init_settings.test = False
+
+
+@fixture
+def simple_app() -> Generator[FastAPI, None, None]:
+    """Yield a uEditor application for a simple uEdition."""
+    try:
+        init_settings.base_path = "tests/fixtures/simple"
+        init_settings.test = True
+        yield app
+    finally:
+        init_settings.base_path = "./"
+        init_settings.test = False
+
+
+@fixture
+def tei_app() -> Generator[FastAPI, None, None]:
+    """Yield a uEditor application for a uEdition with some TEI."""
+    try:
+        init_settings.base_path = "tests/fixtures/tei"
+        init_settings.test = True
+        yield app
+    finally:
+        init_settings.base_path = "./"
+        init_settings.test = False
--- tests/functional_tests/settings_test.py
+++ tests/functional_tests/settings_test.py
@@ -7,7 +7,7 @@ from ueditor.settings import get_settings, init_settings
 
 def test_basic_env_settings(simple_app: FastAPI) -> None:  # noqa: ARG001
     """Test that the basic env settings are loaded."""
-    assert init_settings.base_path == "./"
+    assert init_settings.base_path == "tests/fixtures/simple"
 
 
 def test_empty_yaml_settings(empty_app: FastAPI) -> None:  # noqa: ARG001
