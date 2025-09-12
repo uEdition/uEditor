97fbb2ff441ef2c93c94f9a7ccbe95d48f0cca21
Mark Hall <mark.hall@work.room3b.eu>
2025-09-12 14:20:18 +0100

    Add PDF Viewer

---
+++ uedition_editor/frontend/src/lib/editors/PdfEditor.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { useCurrentBranch, currentFile } from "../../stores";
+
+  const currentBranch = useCurrentBranch();
+</script>
+
+<div class="overflow-auto w-full">
+  {#if $currentBranch !== null && $currentFile}
+    <iframe
+      src="/api/branches/{$currentBranch.id}/files/{$currentFile.fullpath}"
+      title="Test"
+      class="w-full h-full"
+    />
+  {/if}
+</div>
